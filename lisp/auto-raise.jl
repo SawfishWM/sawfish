95699209cd736ed8018ea88be01f3b569de2cd13
john <john>
1999-07-29 13:42:05 +0000

    new file, uses a timer to work correctly
    (raise-windows-on-focus): can now be a regexp matching window names to
    raise

---
+++ lisp/auto-raise.jl
@@ -0,0 +1,55 @@
+;; auto-raise.jl -- auto-raise on focus
+;; $Id$
+
+;; Copyright (C) 1999 John Harper <john@dcs.warwick.ac.uk>
+
+;; This file is part of sawmill.
+
+;; sawmill is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawmill is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawmill; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(require 'timers)
+(provide 'focus)
+
+(defvar raise-windows-on-focus t
+  "When non-nil, windows are raised after receiving focus. If a string,
+windows are only raised if there name matches this regular expression.")
+
+(defvar raise-window-timeout 500
+  "Time in milliseconds until windows are raised if raise-windows-on-focus
+is set.")
+
+(defvar rw-timer nil)
+(defvar rw-window nil)
+
+(defun rw-on-focus (w)
+  (if (or (and (stringp raise-windows-on-focus)
+	       (string-match raise-windows-on-focus (window-name w)))
+	  raise-windows-on-focus)
+      (progn
+	(setq rw-window w)
+	(if rw-timer
+	    (set-timer rw-timer)
+	  (setq rw-timer (make-timer 'rw-timer-callback
+				     (/ raise-window-timeout 1000)
+				     (mod raise-window-timeout 1000)))))
+    (when rw-timer
+      (delete-timer rw-timer)
+      (setq rw-timer nil))))
+
+(defun rw-timer-callback ()
+  (raise-window rw-window)
+  (setq rw-timer nil))
+
+(add-hook 'focus-in-hook 'rw-on-focus)
