232dfb115488fea8871a602b27e2db0e99e27610
john <john>
1999-08-02 09:51:49 +0000

    (place-window): fix a missing close-parenthesis

--- lisp/sawfish/wm/placement.jl
+++ lisp/sawfish/wm/placement.jl
@@ -35,24 +35,24 @@
 	    (cdr (assq 'user-position hints))
 	    (and (not ignore-program-positions)
 		 (cdr (assq 'program-position hints))))
-      nil
-    (let
-	((mode (or (window-get w 'place-mode) place-window-mode)))
-      (cond ((eq mode 'smart)
-	     ;; XXX implement this..
-	     (setq mode 'random))
-;	    ((eq mode 'interactive)
-;	     ;; XXX this doesn't work; why not?
-;	     (let
-;		 ((move-outline-mode nil))
-;	       (move-window-interactively w)))
-	    ((eq mode 'random)
-	     (move-window-to
-	      w
-	      (random (max 0 (- (screen-width)
-				(car (window-dimensions w)))))
-	      (random (max 0 (- (screen-height)
-				(cdr (window-dimensions w))))))))
-      t)))
+	nil
+      (let
+	  ((mode (or (window-get w 'place-mode) place-window-mode)))
+	(cond ((eq mode 'smart)
+	       ;; XXX implement this..
+	       (setq mode 'random))
+	      ((eq mode 'interactive)
+	       ;; XXX this doesn't work; why not?
+	       (let
+		   ((move-outline-mode nil))
+		 (move-window-interactively w)))
+	      ((eq mode 'random)
+	       (move-window-to
+		w
+		(random (max 0 (- (screen-width)
+				  (car (window-dimensions w)))))
+		(random (max 0 (- (screen-height)
+				  (cdr (window-dimensions w))))))))
+	t))))
 
 (add-hook 'place-window-hook 'place-window t)
