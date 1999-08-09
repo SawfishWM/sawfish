15ba2e51345b27b4d8ad32e632d86dcd558480c8
john <john>
1999-08-09 15:19:43 +0000

    remove some settings that are normally set by customize

--- lisp/sawfish/wm/defaults.jl
+++ lisp/sawfish/wm/defaults.jl
@@ -19,6 +19,8 @@
 ;; along with sawmill; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
+;; Most of these settings can be overridden through customize
+
 ;; open the server, use the sawmill-client program to connect to the
 ;; running window manager
 (server-open)
@@ -29,23 +31,8 @@
 ;; the font to use for otherwise unspecified text
 (setq default-font (get-font "-*-lucida-medium-r-*-*-10-*-*-*-*-*-*-*"))
 
-;; load a theme, take your pick from the few choices..
-(load "absolute-e")
-;(load "brushed-metal")
-;(load "simple")
-
-;; ignore windows matching these regexps
-(setq ignored-window-names
-      (cons "^(xload|xmeter|Dali Clock|xapm|xbuffy|console)$"
-	    ignored-window-names))
-
-;; these windows are sticky
-(setq sticky-window-names
-      (cons "^(xload|xmeter|Dali Clock|xapm|xbuffy|console)$"
-	    sticky-window-names))
-
-;; if you use GNOME, uncomment this
-;(require 'gnome)
+;; if you don't use GNOME, remove this (though it does no harm)
+(require 'gnome)
 
 ;; alternatively, this will probably work if you use GDM, but only
 ;; sometimes use GNOME
