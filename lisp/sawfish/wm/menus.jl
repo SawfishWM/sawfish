20c77e8d03933b677383546933a84c289da8982f
john <john>
1999-10-22 19:46:05 +0000

    set frame-draw-mutex while menus are popped up

--- lisp/sawfish/wm/menus.jl
+++ lisp/sawfish/wm/menus.jl
@@ -167,6 +167,7 @@ unused before killing it.")
       ((orig-win menu-active))
     (menu-stop-process)
     (setq menu-active nil)
+    (setq frame-draw-mutex nil)
     (when result
       (when (windowp orig-win)
 	(current-event-window orig-win))
@@ -187,6 +188,9 @@ unused before killing it.")
 	 (dims (clicked-frame-part-dimensions)))
       (setq menu-active (or (current-event-window) (input-focus)))
       (menu-start-process)
+      ;; prevent any depressed button being redrawn until the menu
+      ;; is popped down
+      (setq frame-draw-mutex t)
       ;; This function is probably called from a ButtonPress event,
       ;; so cancel the implicit pointer grab (to allow the menu's grab
       ;; to succeed)
