6c0b4b0eb78a4221e916ab4af4d5efff5090ad33
john <john>
1999-12-08 15:39:44 +0000

    (raise-window-and-pass-through-click): call forget-button-press so that double-clicks pass through okay

--- lisp/stacking.jl
+++ lisp/stacking.jl
@@ -188,7 +188,8 @@ events that invoked the command."
   (interactive "%w")
   (when (windowp w)
     (raise-window w))
-  (allow-events 'replay-pointer))
+  (allow-events 'replay-pointer)
+  (forget-button-press))
 
 (add-hook 'after-initialization-hook restack-by-depth)
 (add-hook 'add-window-hook stacking-add-window t)
