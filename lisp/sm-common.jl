4a9d29b1e777e1ef89fdf310b7d90a0c844c6b0f
john <john>
1999-12-08 22:30:51 +0000

    fix for new unless

--- lisp/sm-common.jl
+++ lisp/sm-common.jl
@@ -28,14 +28,14 @@
 ;; find PROP associated with W, or nil
 (defun sm-get-window-prop (w prop)
   ;; first look in the window itself,
-  (unless (nth 2 (get-x-property w prop))
-    ;; else try the leader
-    (let*
-	(tem
-	 (leader (cond ((and (setq tem (get-x-property w 'WM_CLIENT_LEADER))
-			     (eq (car tem) 'WINDOW)
-			     (eq (nth 1 tem) 32))
-			(aref (nth 2 tem) 0))
-		       ((window-group-id w))
-		       ((window-transient-p w)))))
-      (and leader (nth 2 (get-x-property leader prop))))))
+  (or (nth 2 (get-x-property w prop))
+      ;; else try the leader
+      (let*
+	  (tem
+	   (leader (cond ((and (setq tem (get-x-property w 'WM_CLIENT_LEADER))
+			       (eq (car tem) 'WINDOW)
+			       (eq (nth 1 tem) 32))
+			  (aref (nth 2 tem) 0))
+			 ((window-group-id w))
+			 ((window-transient-p w)))))
+	(and leader (nth 2 (get-x-property leader prop))))))
