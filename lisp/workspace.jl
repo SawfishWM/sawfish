7d3157c995ebcdf0e25036521d2090d10293778b
john <john>
1999-12-17 14:50:49 +0000

    use closures to create dynamic commands; moved iconification to separate file

--- lisp/workspace.jl
+++ lisp/workspace.jl
@@ -797,125 +797,23 @@ previous workspace."
 ;; some commands for moving directly to a workspace
 
 (let
-    ((i 1))
-  (while (< i 10)
-    (set (intern (format nil "select-workspace:%s" i))
-	 (make-closure `(lambda ()
+    ((define-commands
+      (lambda (index)
+	(let
+	    ((fn (lambda (base)
+		   (intern (format nil "%s:%d" base (1+ index))))))
+	  (define-value (fn "select-workspace")
+			(lambda ()
 			  (interactive)
-			  (select-workspace-from-first ,(1- i)))))
-    (set (intern (format nil "send-to-workspace:%s" i))
-	 (make-closure `(lambda (w)
+			  (select-workspace-from-first index)))
+	  (define-value (fn "send-to-workspace")
+			(lambda (w)
 			  (interactive "%W")
-			  (send-window-to-workspace-from-first w ,(1- i)))))
+			  (send-window-to-workspace-from-first w index))))))
+     (i 0))
+  (while (< i 9)
+    (define-commands i)
     (setq i (1+ i))))
-
-
-;; Iconification (but without icons)
-
-;; If iconified, a window has its `iconified' property set to t
-
-(defun iconify-window (w)
-  "Iconify the window."
-  (interactive "%W")
-  (unless (window-get w 'iconified)
-    (window-put w 'iconified t)
-    (when (window-visible-p w)
-      (hide-window w))
-    (call-window-hook 'iconify-window-hook w)
-    (call-window-hook 'window-state-change-hook w)
-    (when iconify-whole-group
-      (iconify-group w))))
-
-(defun uniconify-window (w)
-  "Return the window from its iconified state."
-  (interactive "%W")
-  (when (window-get w 'iconified)
-    (window-put w 'iconified nil)
-    (cond ((or (window-get w 'sticky)
-	       (window-in-workspace-p w current-workspace))
-	   (show-window w))
-	  (uniconify-to-current-workspace
-	   (ws-remove-window w)
-	   (ws-add-window-to-space w current-workspace)))
-    (when raise-windows-on-uniconify
-      (raise-window w))
-    (when (and focus-windows-on-uniconify (window-really-wants-input-p w))
-      (set-input-focus w))
-    (call-window-hook 'uniconify-window-hook w)
-    (call-window-hook 'window-state-change-hook w)
-    (when uniconify-whole-group
-      (uniconify-group w))))
-
-(defun display-window (w &optional preferred-space)
-  "Display the workspace containing the window W, then focus on W."
-  (interactive "%W")
-  (when w
-    (if (and (window-get w 'iconified)
-	     (or uniconify-to-current-workspace (window-get w 'sticky)))
-	(uniconify-window w)
-      (when (or (not preferred-space)
-		(not (window-in-workspace-p w preferred-space)))
-	(setq preferred-space
-	      (nearest-workspace-with-window w current-workspace)))
-      (when preferred-space
-	(select-workspace preferred-space))
-      (move-viewport-to-window w)
-      (uniconify-window w)
-      (when (and unshade-selected-windows (window-get w 'shaded))
-	(unshade-window w))
-      (when raise-selected-windows
-	(raise-window w))
-      (when warp-to-selected-windows
-	(warp-cursor-to-window w))
-      (when (window-really-wants-input-p w)
-	(set-input-focus w))
-      (window-order-push w))))
-
-(defun make-window-sticky (w)
-  (interactive "%W")
-  (unless (window-get w 'sticky)
-    (ws-remove-window w t)
-    (window-put w 'sticky t)
-    (window-put w 'sticky-viewport t)
-    (call-window-hook 'window-state-change-hook w)))
-  
-(defun make-window-unsticky (w)
-  (interactive "%W")
-  (when (window-get w 'sticky)
-    (window-put w 'sticky nil)
-    (window-put w 'sticky-viewport nil)
-    (ws-add-window-to-space w current-workspace)
-    (call-window-hook 'window-state-change-hook w)))
-  
-(defun toggle-window-sticky (w)
-  "Toggle the `stickiness' of the window--whether or not it is a member of
-all workspaces."
-  (interactive "%W")
-  (if (window-get w 'sticky)
-      (make-window-unsticky w)
-    (make-window-sticky w)))
-
-(defun ws-client-msg-handler (w prop data)
-  (cond ((and (windowp w)
-	      (eq prop 'WM_CHANGE_STATE)
-	      (= (aref data 0) IconicState))
-	 (iconify-window w)
-	 t)))
-
-(defun ws-honour-client-state (w)
-  (let
-      ((state (get-x-property w 'WM_STATE)))
-    (when state
-      (setq state (aref (nth 2 state) 0))
-      (when (eq state IconicState)
-	(window-put w 'iconified t)))))
-
-(defun ws-set-client-state (w)
-  (set-x-property w 'WM_STATE
-		  (vector (if (window-get w 'iconified)
-			      IconicState
-			    NormalState) 0)
-		  'WM_STATE 32))
 
 
 ;; session management
@@ -995,14 +893,9 @@ all workspaces."
 ;; some of these should really be added by other files
 (add-swapped-properties 'frame-style 'type 'hide-client 'iconified 'order)
 
-(unless (or batch-mode (memq 'ws-add-window add-window-hook))
-  (add-hook 'add-window-hook ws-add-window)
-  (add-hook 'map-notify-hook ws-window-mapped)
-  (add-hook 'unmap-notify-hook ws-window-unmapped)
-  (add-hook 'destroy-notify-hook ws-remove-window)
-  (add-hook 'client-message-hook ws-client-msg-handler)
-  (add-hook 'before-add-window-hook ws-honour-client-state)
-  (add-hook 'add-window-hook ws-set-client-state t)
-  (add-hook 'window-state-change-hook ws-set-client-state)
-  (add-hook 'sm-window-save-functions ws-saved-state)
-  (add-hook 'sm-restore-window-hook ws-load-state))
+(add-hook 'add-window-hook ws-add-window)
+(add-hook 'map-notify-hook ws-window-mapped)
+(add-hook 'unmap-notify-hook ws-window-unmapped)
+(add-hook 'destroy-notify-hook ws-remove-window)
+(add-hook 'sm-window-save-functions ws-saved-state)
+(add-hook 'sm-restore-window-hook ws-load-state)
