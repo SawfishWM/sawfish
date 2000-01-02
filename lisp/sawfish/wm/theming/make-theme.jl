2dfd1df137802056cbc697c0e630ccd4c616083a
john <john>
2000-01-02 23:57:24 +0000

    image decls now have attributes

--- lisp/sawfish/wm/theming/make-theme.jl
+++ lisp/sawfish/wm/theming/make-theme.jl
@@ -1,5 +1,23 @@
-;; make-theme.jl -- 
+;; make-theme.jl -- support for theme builder
 ;; $Id$
+
+;; Copyright (C) 2000 John Harper <john@dcs.warwick.ac.uk>
+
+;; This file is part of sawmill.
+
+;; sawmill is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawmill is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawmill; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
 (require 'gaol)
 (provide 'make-theme)
@@ -9,10 +27,23 @@
       ((make-pattern
 	(lambda (def)
 	  (mapcar (lambda (x)
-		    (when (stringp x)
-		      (if (string-match "^#" x)
-			  (get-color x)
-			(gaol-eval `(make-image ',x))))) def)))
+		    (cond ((stringp x)
+			   (get-color x))
+			  ((and (consp x) (stringp (car x)))
+			   (let
+			       ((img (gaol-eval `(make-image ',(car x)))))
+			     (when img
+			       (mapc (lambda (attr)
+				       (cond
+					((eq (car attr) 'tiled)
+					 (image-put img 'tiled (cdr attr)))
+					((eq (car attr) 'border)
+					 (apply set-image-border
+						img (cdr attr)))))
+				     (cdr x)))
+			     img))))
+		  def)))
+
        (loaded-patterns
 	(mapcar (lambda (cell)
 		  (cons (car cell) (make-pattern (cdr cell))))
