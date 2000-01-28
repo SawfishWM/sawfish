423d142491fbdeaf209c51be18b20439e102ab00
john <john>
2000-01-28 17:40:28 +0000

    new file, temporarily unshades windows while the pointer is over their frame

---
+++ lisp/sawfish/wm/ext/shade-hover.jl
@@ -0,0 +1,59 @@
+;; shade-hover.jl -- temporarily unshade windows while hovered over
+;; $Id$
+
+;; Copyright (C) 2000 John Harper <john@dcs.warwick.ac.uk>
+
+;; This file is part of sawmill.
+
+;; sawmill is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawmill is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawmill; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(require 'timers)
+(provide 'shade-hover)
+
+(defvar shade-hover-delay 250)
+
+(defvar shade-hover-timer nil)
+
+(defun shade-hover-fp-leave (w fp)
+  (when shade-hover-timer
+    (delete-timer shade-hover-timer)
+    (setq shade-hover-timer nil))
+  (when (window-get w 'shade-hover-unshaded)
+    (window-put w 'shade-hover-unshaded nil)
+    (shade-window w)))
+
+(defun shade-hover-fp-enter (w fp)
+  (when (window-get w 'shaded)
+    (let
+	((callback
+	  (lambda ()
+	    (window-put w 'shade-hover-unshaded t)
+	    (unshade-window w)))
+	 (pre-command-callback
+	  (lambda ()
+	    (let
+		((command (lookup-event-binding (current-event))))
+	      (unless (eq command 'toggle-window-shaded)
+		(window-put w 'shade-hover-unshaded nil))
+	      (shade-hover-fp-leave w fp)))))
+      (when shade-hover-timer
+	(delete-timer shade-hover-timer))
+      (setq shade-hover-timer (make-timer callback
+					  (/ shade-hover-delay 1000)
+					  (mod shade-hover-delay 1000)))
+      (add-hook 'pre-command-hook pre-command-callback))))
+
+(add-hook 'enter-frame-part-hook shade-hover-fp-enter)
+(add-hook 'leave-frame-part-hook shade-hover-fp-leave)
