43a61a2724739efd5742e4190403a3841a2a984a
john <john>
2000-02-03 17:14:32 +0000

    new file

---
+++ lisp/sawfish/wm/custom-defaults.jl
@@ -0,0 +1,22 @@
+;; custom-defaults.jl -- customize settings loaded if no user customization
+;; $Id$
+
+;; Copyright (C) 2000 John Harper <john@dcs.warwick.ac.uk>
+
+;; This file is part of sawmill.
+
+;; sawmill is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawmill is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawmill; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(custom-set-variable 'tooltips-enabled t 'tooltips)
