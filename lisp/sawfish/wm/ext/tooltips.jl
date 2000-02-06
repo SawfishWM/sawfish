f38af599d7235b5466a007dcc8e56e185215390b
john <john>
2000-02-06 21:38:18 +0000

    option to display actual command documentation in tooltips. Use a monospaced font by default, and align the columns correctly

--- lisp/sawfish/wm/ext/tooltips.jl
+++ lisp/sawfish/wm/ext/tooltips.jl
@@ -37,6 +37,11 @@
   :type boolean
   :group tooltips)
 
+(defcustom tooltips-show-doc-strings t
+  "Show full documentation in tooltips."
+  :type boolean
+  :group tooltips)
+
 (defcustom tooltips-delay 500
   "Number of milliseconds before displaying tooltips."
   :type number
@@ -48,6 +53,12 @@
   :type number
   :range (0 . nil)
   :group tooltips)
+
+(defcustom tooltips-font
+ (get-font "-*-lucidatypewriter-medium-*-*-*-10-*-*-*-*-*-*-*")
+ "Font used to display tooltips."
+ :type font
+ :group tooltips)
 
 (defvar tooltips-background-color "lightyellow2")
 
@@ -63,29 +74,71 @@
     (delete-timer tooltips-timer)
     (setq tooltips-timer nil)))
 
+;; each item is (EVENT-DESC . DOC)
+(defun tooltips-format (items)
+  (let
+      ((max-event-width 0)
+       (split (lambda (s)
+		(let
+		    (point parts)
+		  (while (string-match "\n" s point)
+		    (setq parts (cons (substring
+				       s (or point 0) (match-start)) parts))
+		    (setq point (match-end)))
+		  (setq parts (cons (substring s (or point 0)) parts))
+		  (nreverse parts))))
+       out)
+    (mapc (lambda (cell)
+	    (setq max-event-width (max max-event-width (length (car cell)))))
+	  items)
+    (setq max-event-width (1+ max-event-width))
+    (mapc (lambda (cell)
+	    (setq out (cons (car cell) out))
+	    (setq out (cons (make-string (- max-event-width
+					    (length (car cell))) ? ) out))
+	    (let
+		((parts (split (cdr cell))))
+	      (when parts
+		(setq out (cons (car parts) out))
+		(setq out (cons ?\n out)))
+	      (mapc (lambda (string)
+		      (setq out (cons (make-string max-event-width ? ) out))
+		      (setq out (cons string out))
+		      (setq out (cons ?\n out))) (cdr parts)))) items)
+    (apply concat (nreverse out))))
+
 (defun tooltips-display (win fp)
   (let
-      ((text (make-string-output-stream))
-       (keymap (frame-part-get fp 'keymap))
+      ((keymap (frame-part-get fp 'keymap))
        (pos (query-pointer))
        (pos-fn (lambda (in size inc)
 		 (if (< in (/ size 2))
 		     (+ in inc)
-		   (- (+ (- size in) inc))))))
+		   (- (+ (- size in) inc)))))
+       (doc-fn (lambda (command)
+		 (let
+		     (doc)
+		   (if (and tooltips-show-doc-strings command
+			    (symbolp command)
+			    (setq doc (documentation command)))
+		       (_ doc)
+		     (format nil "%S" command)))))
+       items)
     (when (symbolp keymap)
       (setq keymap (symbol-value keymap)))
     (map-keymap (lambda (cell)
-		  (format text "<%s>   %S\n"
-			  (event-name (cdr cell)) (car cell)))
+		  (setq items (cons (cons (event-name (cdr cell))
+					  (doc-fn (car cell))) items)))
 		keymap)
-    (setq text (get-output-stream-string text))
-    (unless (string= text "")
+    (when items
       (rplaca pos (pos-fn (car pos) (screen-width) 0))
       (rplacd pos (pos-fn (cdr pos) (screen-height) 16))
-      (display-message text `((position . ,pos)
-			      (background . ,tooltips-background-color)
-			      (x-justify . left)
-			      (spacing . 2)))
+      (display-message (tooltips-format (nreverse items))
+		       `((position . ,pos)
+			 (background . ,tooltips-background-color)
+			 (x-justify . left)
+			 (spacing . 2)
+			 (font . ,tooltips-font)))
       (setq tooltips-displayed t)
       (when tooltips-timeout-enabled 
 	(setq tooltips-timer (make-timer tooltips-cleanup
