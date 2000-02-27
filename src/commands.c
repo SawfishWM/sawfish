7c07e5cb538f03065bed51ac1a02016434dc92d7
john <john>
2000-02-27 16:05:05 +0000

    *** empty log message ***

--- src/commands.c
+++ src/commands.c
@@ -320,7 +320,10 @@ Returns the numeric value of the raw prefix argument ARG.
     case rep_Symbol:
 	arg = rep_MAKE_INT(rep_NILP(arg) ? 1 : -1);
 	break;
-    case rep_Int: case rep_Number:
+    case rep_Int:
+#ifdef rep_Number
+    case rep_Number:
+#endif
 	break;
     case rep_Cons:
 	arg = rep_CAR(arg);
