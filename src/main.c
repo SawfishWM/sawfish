f554e9018dac7cc0847b4d84487d78e95e4389a1
john <john>
2000-04-23 11:59:12 +0000

    (main): set the sawmill structure to export all bindings

--- src/main.c
+++ src/main.c
@@ -332,6 +332,7 @@ main(int argc, char **argv)
 
 #if rep_INTERFACE >= 9
     rep_push_structure ("sawmill");
+    rep_structure_exports_all (rep_structure, rep_TRUE);
     rep_INTERN (rep);
     Frequire (Qrep);
 #endif
