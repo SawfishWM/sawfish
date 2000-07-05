3108c9835ca64032c5b6e28277581846e69ccaee
john <john>
2000-07-05 13:24:51 +0000

    (monitor-pixbuf): use the local version

--- nokogiri/nokogiri-widgets/workspace-geometry.jl
+++ nokogiri/nokogiri-widgets/workspace-geometry.jl
@@ -1,4 +1,27 @@
+#| nokogiri-widgets/file.jl -- file name widget
 
+   $Id$
+
+   Copyright (C) 2000 John Harper <john@dcs.warwick.ac.uk>
+
+   This file is part of sawfish.
+
+   sawfish is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   sawfish is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with sawfish; see the file COPYING.  If not, write to
+   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+|#
+
+(require 'nokogiri-interfaces)
 
 (define-structure nokogiri-widgets/workspace-geometry ()
 
@@ -13,7 +36,9 @@
   (defconst canvas-width 100)
   (defconst canvas-height 100)
 
-  (defconst monitor-pixbuf-file "/usr/share/pixmaps/monitor.png")	;XXX
+  (define monitor-pixbuf-file
+    (canonical-file-name
+     (expand-file-name "monitor.png" *nokogiri-directory*)))
 
   (define monitor-pixbuf (gdk-pixbuf-new-from-file monitor-pixbuf-file))
 
