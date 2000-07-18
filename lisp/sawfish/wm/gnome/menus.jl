644f2ea5590b59fb81e6a8b6e8581ac2b3bb1a22
john <john>
2000-07-18 17:15:20 +0000

    require 'menus when compiling (otherwise when setting apps-menu, don't know that it's special)

--- lisp/sawfish/wm/gnome/menus.jl
+++ lisp/sawfish/wm/gnome/menus.jl
@@ -19,6 +19,8 @@
 ;; along with sawmill; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 
+(eval-when-compile (require 'menus))
+
 (provide 'gnome-menu)
 
 
