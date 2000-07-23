ed999d99a645003f0b7b23f607398d6e77fc30d7
john <john>
2000-07-23 22:17:05 +0000

    fix structure name in definition

--- nokogiri/nokogiri-widgets/font.jl
+++ nokogiri/nokogiri-widgets/font.jl
@@ -21,7 +21,7 @@
    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 |#
 
-(define-structure nokogiri-widget:font ()
+(define-structure nokogiri-widgets/font ()
 
     (open rep
 	  gtk
