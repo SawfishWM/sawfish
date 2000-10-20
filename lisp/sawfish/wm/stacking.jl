1bd1c8940ba67fe5989f0152d9384a215e9f17f5
john <john>
2000-10-20 23:25:58 +0000

    renamed raise-window, lower-window, and raise-lower-window as raise-single-window, etc..
    put raise/lower commands at 'expert level

--- lisp/sawfish/wm/stacking.jl
+++ lisp/sawfish/wm/stacking.jl
@@ -334,9 +334,12 @@ lowest possible position. Otherwise raise it as far as allowed."
     "Put the window in the stacking level above its current level."
     (set-window-depth w (1+ (window-get w 'depth))))
 
-  (define-command 'raise-window raise-window #:spec "%W")
-  (define-command 'lower-window lower-window #:spec "%W")
-  (define-command 'raise-lower-window raise-lower-window #:spec "%W")
+  (define-command 'raise-single-window raise-window
+    #:spec "%W" #:user-level 'expert)
+  (define-command 'lower-single-window lower-window
+    #:spec "%W" #:user-level 'expert)
+  (define-command 'raise-lower-single-window raise-lower-window
+    #:spec "%W" #:user-level 'expert)
   (define-command 'raise-window-depth raise-window-depth #:spec "%W")
   (define-command 'lower-window-depth lower-window-depth #:spec "%W")
 
