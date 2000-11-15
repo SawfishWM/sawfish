b59501b45fc81b61179a25bd2ed7dad916e866a3
John Harper <jsh@src.gnome.org>
2000-11-15 18:52:05 +0000

    don't call window-order-focus-most-recent if using the `enter-exit' focus
    	* sawfish/wm/util/window-order.jl (on-viewport-change): don't
    	call window-order-focus-most-recent if using the `enter-exit'
    	focus mode

--- lisp/sawfish/wm/util/window-order.jl
+++ lisp/sawfish/wm/util/window-order.jl
@@ -107,7 +107,8 @@ function will restrict its search to the elements of this list."
       (mapc (lambda (w)
 	      (when (window-get w 'sticky-viewport)
 		(window-put w 'order nil))) order))
-    (window-order-focus-most-recent))
+    (unless (eq focus-mode 'enter-exit)
+      (window-order-focus-most-recent)))
 
   (sm-add-saved-properties 'order)
   (add-swapped-properties 'order)
