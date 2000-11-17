62aacc01209593e447213747cc2c5d5125e9c219
John Harper <jsh@src.gnome.org>
2000-11-17 04:10:44 +0000

    set the `no-history' property of panel windows
    	* sawfish/wm/state/gnome.jl (gnome-honour-client-state): set
    	the `no-history' property of panel windows

--- lisp/sawfish/wm/state/gnome.jl
+++ lisp/sawfish/wm/state/gnome.jl
@@ -186,6 +186,7 @@
 		    (string= (aref class 0) "panel"))
 	       ;; XXX I don't think the GNOME hints specify these things...
 	       (window-put w 'focus-click-through t)
+	       (window-put w 'no-history t)
 	       ;; XXX the panel should set this, but sometimes it fails..?
 	       (window-put w 'avoid t))
 	      ((string= (aref class 1) "gmc-desktop-icon")
