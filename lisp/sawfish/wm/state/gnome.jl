23c8ac651abc77299114e8b195e9cf3a41b2c070
John Harper <jsh@pixelslut.com>
2001-07-24 04:37:14 +0000

    fixed bug caused by Click1 events now printing as Click. This caused only
    2001-07-23  John Harper  <jsh@pixelslut.com>
    
    	* sawfish/wm/state/gnome.jl (gnome-event-proxyer): fixed bug
    	caused by Click1 events now printing as Click. This caused only
    	button-release events to be proxied

--- lisp/sawfish/wm/state/gnome.jl
+++ lisp/sawfish/wm/state/gnome.jl
@@ -321,7 +321,7 @@
       (let ((event (event-name (current-event))))
 	;; only proxy Click1 or Off events, and only if we don't have
 	;; a binding for an event that may follow in the same grab
-	(cond ((and (string-match "^(.*)-Click1$" event)
+	(cond ((and (string-match "^(.*)-Click\\d?$" event)
 		    (let ((mirror (lookup-event
 				   (expand-last-match "\\1-Off"))))
 		      (not (or (search-keymap mirror global-keymap)
