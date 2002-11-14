c0620d844ce83105a9bde4e042b00e7f5d087ad8
John Harper <jsh@unfactored.org>
2002-11-14 06:58:00 +0000

    fixed typo when accessing window-frame-offset. Fixes #98315
    2002-11-13  John Harper  <jsh@unfactored.org>
    
    	* sawfish/wm/windows.jl (adjust-position-for-gravity/y): fixed
    	typo when accessing window-frame-offset. Fixes #98315

--- lisp/sawfish/wm/windows.jl
+++ lisp/sawfish/wm/windows.jl
@@ -320,7 +320,7 @@ If HINTS is non-nil, then it is the size hints structure to use. Otherwise
 	    (t x))))
 
   (define (adjust-position-for-gravity/y w grav y #!key inverse)
-    (let* ((tl-off (window-frame-offset w))
+    (let* ((tl-off (cdr (window-frame-offset w)))
 	   (br-off (- (cdr (window-frame-dimensions w))
 		      (cdr (window-dimensions w))))
 	   (sign (if inverse -1 +1)))
