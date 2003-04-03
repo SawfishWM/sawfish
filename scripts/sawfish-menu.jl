ff1e35258bed6044496e13ab4d67b8be67d78db1
John Harper <jsh@unfactored.org>
2003-04-03 02:23:33 +0000

    handle case where menu doesn't get realized and so we shouldn't enter
    2003-04-02  John Harper  <jsh@unfactored.org>
    
    	* sawfish-menu.jl: handle case where menu doesn't get realized
    	and so we shouldn't enter gtk-main. Fixes longstanding (for
    	ever!) "menu already active" bug (#89139). Patch from Jan Hudec
    	<bulb@ucw.cz>

--- scripts/sawfish-menu.jl
+++ scripts/sawfish-menu.jl
@@ -106,7 +106,8 @@ exec rep "$0" "$@"
     (g-signal-connect menu "deactivate" gtk-main-quit)
     (setq menu-selected nil)
     (gtk-menu-popup-interp menu nil nil 0 (or timestamp 0) position)
-    (gtk-main)
+    (when (memq 'visible (GTK-WIDGET-FLAGS menu))
+      (gtk-main))
     menu-selected))
 
 
