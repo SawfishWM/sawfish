ffc9f0aeea643a23a3bb2d874dd97ed5ffc25dd2
John Harper <jsh@unfactored.org>
2003-10-30 05:35:18 +0000

    patch from David Mosberger to fix reading `negative' unsigned values on
    2003-10-29  John Harper  <jsh@unfactored.org>
    
    	* functions.c (Fget_x_property): patch from David Mosberger to
    	fix reading `negative' unsigned values on 64-bit platforms
    	(with a special case I added to read XA_INTEGER properties)

--- src/functions.c
+++ src/functions.c
@@ -704,8 +704,10 @@ symbols, representing the atoms read.
 	    repv name;
 	    if (type == XA_ATOM && (name = x_atom_symbol (l_data[i])) != Qnil)
 		rep_VECTI(ret_data, i) = name;
+	    else if (type == XA_INTEGER)
+		rep_VECTI(ret_data, i) = rep_make_long_int((long) l_data[i]);
 	    else
-		rep_VECTI(ret_data, i) = rep_make_long_uint(l_data[i]);
+		rep_VECTI(ret_data, i) = rep_make_long_uint(l_data[i] & 0xffffffffUL);
 	}
 	break;
     }
