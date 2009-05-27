cb5213172e5adfb4f9d5996d58bc41af7a62480f
chrisb <zanghar@freenet.de>
2009-05-27 22:13:10 +0200

    lisp/sawfish/wm/util/prompt.jl: fixed typo

--- lisp/sawfish/wm/util/prompt.jl
+++ lisp/sawfish/wm/util/prompt.jl
@@ -6,7 +6,7 @@
 ;;   
 ;; This file is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
-:q;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
 ;; Commentary:
