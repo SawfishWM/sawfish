dfc96b9ddc96da7910c85aa7bc0b07ccefc844fa
chrisb <zanghar@freenet.de>
2009-07-26 09:51:45 +0200

    several improvements to sawfish.wm.gnome.integration

--- lisp/sawfish/wm/gnome/integration.jl
+++ lisp/sawfish/wm/gnome/integration.jl
@@ -32,19 +32,8 @@
 
   (define-structure-alias gnome-int sawfish.wm.gnome.integration)
 
-  ;; delete the `Restart' and `Quit' items from the root menu
-  (let ((restart (rassoc '(restart) root-menu))
-	(quit (rassoc '(quit) root-menu)))
-;;	(programs (rassoc 'apps-menu root-menu)))
-    (when restart
-      (setq root-menu (delq restart root-menu)))
-    (when quit
-      (setq root-menu (delq quit root-menu)))
-    ;; FIXME: until GNOME 2 applications are supported
-;;    (when programs
-;;      (setq root-menu (delq programs root-menu)))
-    (when (null (last root-menu))
-      (setq root-menu (delq (last root-menu) root-menu))))
+  (when (null (last root-menu))
+    (setq root-menu (delq (last root-menu) root-menu)))
 
   ;; this option was removed for gnome2
   (put 'gnome-use-capplet 'custom-obsolete t)
@@ -55,7 +44,7 @@
 
   ;; use the GNOME help browser and url launcher
   (setq help-display-info-function help-call-info-gnome)
-  (setq display-url-command "gnome-moz-remote --newwin '%s'")
+  (setq display-url-command "gnome-www-browser %s &")
 
   ;; add some GNOME help menus
   (let ((menu (assoc (_ "_Help") root-menu)))
