a337e66e659e15ef458f553af62ec7c91c422392
chrisb <zanghar@freenet.de>
2009-09-03 19:40:31 +0200

    added (initial) kde integration module

---
+++ lisp/sawfish/wm/integration/kde.jl
@@ -0,0 +1,59 @@
+;; gnome-int.jl -- more GNOME integration
+;; $Id: integration.jl,v 1.20 2003/08/14 06:55:36 jsh Exp $
+
+;; Copyright (C) 2000 John Harper <john@dcs.warwick.ac.uk>
+
+;; This file is part of sawfish.
+
+;; sawfish is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawfish is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawfish; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+(define-structure sawfish.wm.integration.kde ()
+
+    (open rep
+	  sawfish.wm.menus
+	  sawfish.wm.custom
+	  sawfish.wm.commands
+	  sawfish.wm.commands.user)
+
+  (define-structure-alias kde-int sawfish.wm.integration.kde)
+
+  ;; invoke the KDE terminal instead of xterm
+  (unless (variable-customized-p 'xterm-program)
+    (setq xterm-program "konsole"))
+
+  ;; use the KDE Browser
+  (unless (variable-customized-p 'browser-program)
+    (setq browser-program "konqueror"))
+
+  ;; add the KDE Control Center menu-item
+  (let ((menu (assoc (_ "_Customize") root-menu)))
+    (when menu
+      (nconc menu `(()
+                    (, (_ "_KDE Control Center") (system "systemsettings &"))))))
+
+  ;; add some KDE help menus
+  (let ((menu (assoc (_ "_Help") root-menu)))
+    (when menu
+      (nconc menu `(()
+		    (,(_ "_KDE Help") (system "khelpcenter &"))
+		    (,(_ "KDE Website") (browser "http://www.kde.org")))))))
+		    ;(,(_ "About KDE") (system "false &"))))))
+
+  ;; add kde-logout menu item
+  ;(let ((menu (assoc (_ "Sessi_on") root-menu)))
+  ;  (when menu
+  ;    (nconc menu `(()
+  ;                 (,(_ "_Logout from KDE") (system "false &"))
+;		   (,(_ "_Shutdown from KDE") (system "false &")))))))
