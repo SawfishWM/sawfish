47299a5d2e4b814a2bc5a0cb7379311e1806dffd
Michal Maruska <mmaruska@gmail.com>
2010-11-03 12:18:03 +0200

    leave_notify enter_notify (warn about segfault bug)

--- src/events.c
+++ src/events.c
@@ -970,6 +970,9 @@ enter_notify (XEvent *ev)
 	repv tem;
 	bool refresh = FALSE;
 	Lisp_Window *w = fp->win;
+        if (!w)
+            DB(("%s%s: fp->win = 0 -> %s This would be a SEGFAULT!\n", __FUNCTION__, error_color,
+                color_reset));
 	if (!fp->highlighted && !frame_state_mutex)
 	{
 	    fp->highlighted = 1;
@@ -1016,6 +1019,9 @@ leave_notify (XEvent *ev)
 	repv tem;
 	bool refresh = FALSE;
 	Lisp_Window *w = fp->win;
+         if (!w)
+             DB(("%s%s: fp->win = 0 -> %s This would be a SEGFAULT!\n", __FUNCTION__, error_color,
+                 color_reset));
 	if (fp->highlighted && !frame_state_mutex)
 	{
 	    fp->highlighted = 0;
