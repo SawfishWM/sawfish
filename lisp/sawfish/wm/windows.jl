2b559e6b84e4cd4cd0d409c169a00439593a531a
Christopher Roy Bratusek <zanghar@freenet.de>
2010-11-10 21:32:33 +0100

    reduce rename-window-func to props that actually work

--- lisp/sawfish/wm/windows.jl
+++ lisp/sawfish/wm/windows.jl
@@ -571,11 +571,7 @@ STATES has been changed. STATES may also be a single symbol."
 
   (define (rename-window-func window new-name)
       (set-x-text-property window 'WM_NAME (vector new-name))
-      (set-x-text-property window 'WM_ICON_NAME (vector new-name))
-      ;; XXX doesn't have any effect?
-      (set-x-text-property window '_NET_WM_VISIBLE_NAME (vector new-name))
-      (set-x-text-property window '_NET_WM_NAME (vector new-name))
-      (set-x-text-property window '_NET_WM_ICON_NAME (vector new-name)))
+      (set-x-text-property window 'WM_ICON_NAME (vector new-name)))
 
   (define (rename-window-interactive w)
     (require 'sawfish.wm.util.prompt)
