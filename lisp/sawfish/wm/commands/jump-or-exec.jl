f97d16daed0a226bc67fef61fa701603e9759291
Teika kazura <teika@lavabit.com>
2011-01-08 13:45:54 +0900

    Jump-or-exec cleanup.
    * Wrote a thorough documentation.
    * Handling of arguments "prog" and "onfocused" is extended in lucid way.
      They can be a command-name or a lisp expression, too.
    * Code is cleaned.

--- lisp/sawfish/wm/commands/jump-or-exec.jl
+++ lisp/sawfish/wm/commands/jump-or-exec.jl
@@ -1,4 +1,4 @@
-;;; jump-or-exec.jl 1.0.0 -- flexible application shortcut keys
+;;; jump-or-exec.jl -- flexible application shortcut keys
 
 ;; Copyright (C) 2002 Damien Elmes <resolve@repose.cx>
 ;; Copyright (C) 2010 Christopher Roy Bratusek <zanghar@freenet.de>
@@ -22,22 +22,10 @@
 
 ;;; Description
 
-;; This function either focusses a given window, or starts
-;; the application instead.
+;; Provides key bindings to focus a given window, or starts the
+;; application if absent.
 ;;
-;; Examples:
-;;
-;; => application gnome-run-dialog matched on it's WM_CLASS
-;; ( bind-keys global-keymap "W-F2"
-;;   `( jump-or-exec "Gnome-run-dialog" "gnome-run-dialog" #:match-class t ) )
-;;
-;; => application geany matched on it's WM_NAME
-;; ( bind-keys global-keymap "W-F10"
-;;   `( jump-or-exec "Geany" "geany" #:match-class nil ) )
-;;
-;; NOTE: `#:match-class nil' can be skipped, as it's nil by default
-;;
-;; In version 1.0.0 toggle-or-exec was merged into jump-or-exec. It's basically
+;; toggle-or-exec was merged into jump-or-exec. It's basically
 ;; the same, but it turns windows into drop-down-terminal like ones, that means
 ;; then you press the key while on the corresponding window, it will be hidden.
 ;; Optionally you may add a window-matcher, wich will also hide the window when
@@ -75,26 +63,30 @@
 	  sawfish.wm.util.display-window
 	  sawfish.wm.commands)
 
-  (define (jump-or-exec re prog #!key match-class onfocused)
-    "jump to a window matched by re, or start program otherwise."
-    (catch 'return
-      (let ((wind (if match-class
-                    (get-window-by-class-re re)
-                    (get-window-by-name-re re))))
-        (if (functionp onfocused) ; check if already focused
-            (let ((curwin (input-focus)))
-              (if curwin
-                  (if (or (string-match re (window-class curwin))
-			  (string-match re (window-name curwin)))
-		      (progn
-			;; shouldn't it be curwin
-                        (funcall onfocused wind)
-                        (throw 'return))))))
-        (if (windowp wind)
-            (display-window wind)
-          (if (functionp prog)
-                (funcall prog)
-              (system (concat prog "&")))))))
+  (define (jump-or-exec regex prog #!key match-class onfocused)
+    "Jump to a window, or when absent, start program."
+    (let ((wind (if match-class
+		    (get-window-by-class-re regex)
+		  (get-window-by-name-re regex)))
+	  (curwin (input-focus)))
+      (cond ((and onfocused
+		  curwin
+		  (if match-class
+		      (string-match regex (window-class curwin))
+		    (string-match regex (window-name curwin))))
+	     ;; Exec "onfocused"
+	     (cond ((commandp onfocused)
+		    (call-command onfocused))
+		   ((functionp onfocused)
+		    (funcall onfocused wind))
+		   (t (user-eval onfocused ))))
+	    ;; Jump to the window.
+	    ((windowp wind) (display-window wind))
+	    ;; Exec "prog"
+	    ((stringp prog) (system (concat prog "&")))
+	    ((functionp prog) (funcall prog))
+	    ((commandp prog) (call-command prog))
+	    (t (user-eval prog)))))
 
   (define-command 'jump-or-exec jump-or-exec #:class 'default)
 
@@ -102,15 +94,15 @@
     (let ((default-window-animator 'none))
       (iconify-window (input-focus))))
 
-  (define (toggle-or-exec re prog #!key match-class)
-    (if match-class
-        (jump-or-exec re prog #:match-class t #:onfocused jump-or-exec-leave)
-      (jump-or-exec re prog #:onfocused jump-or-exec-leave)))
+  (define (toggle-or-exec regex prog #!key match-class)
+    (jump-or-exec regex prog
+		  #:match-class match-class
+		  #:onfocused jump-or-exec-leave))
 
   (define (jump-or-exec-hook)
     (if (and (not (eq (current-event-window) 'root)) ;; may error on startup else
 	     (window-get (current-event-window) 'iconify-on-leave))
-      (let ((default-window-animator 'none)) ;; no animator
-	(iconify-window (current-event-window)))))
+	(let ((default-window-animator 'none)) ;; no animator
+	  (iconify-window (current-event-window)))))
 
   (add-hook 'leave-notify-hook jump-or-exec-hook))
