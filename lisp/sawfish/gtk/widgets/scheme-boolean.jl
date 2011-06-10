c9feb42075a48c3a1fc09415a60530d45ac8aec8
Christopher Roy Bratusek <nano@tuxfamily.org>
2011-06-10 21:14:50 +0200

    Revert "re-import b961a052fcedd2545117082b7d3236a84acd49ad (this time it works)"
    This reverts commit f604a8821a940230b590b224fa230c99a38ee3f6.
    
    Conflicts:
    
    	ChangeLog

--- lisp/sawfish/gtk/widgets/scheme-boolean.jl
+++ lisp/sawfish/gtk/widgets/scheme-boolean.jl
@@ -27,19 +27,25 @@
           sawfish.gtk.widget)
 
   (define (make-item changed-callback)
-    (let ((widget (gtk-toggle-button-new-with-label (_ "Enable"))))
+    (let ((widget (gtk-toggle-button-new-with-label (_ "Yes"))))
+      (define (update-label)
+	(gtk-label-set-text (car (gtk-container-get-children widget))
+			    (if (gtk-toggle-button-get-active widget)
+				(_ "Yes") (_ "No"))))
       (gtk-label-set-justify (car (gtk-container-get-children widget)) 'left)
       (gtk-toggle-button-set-active widget t)
       (g-signal-connect
        widget "toggled"
        (lambda ()
+	 (update-label)
 	 (when changed-callback
 	   (call-callback changed-callback))))
       (gtk-widget-show widget)
       (lambda (op)
 	(case op
 	  ((set) (lambda (x)
-		   (gtk-toggle-button-set-active widget x)))
+		   (let ((state (not (memq x '(() #f)))))
+		     (gtk-toggle-button-set-active widget state))))
 	  ((clear) (lambda ()
 		     (gtk-toggle-button-set-active widget nil)))
 	  ((ref) (lambda ()
