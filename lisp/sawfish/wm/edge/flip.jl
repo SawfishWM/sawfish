d0151bc505c770a0b54ac86bc21598aba8fc5569
Christopher Roy Bratusek <nano@tuxfamily.org>
2011-06-27 19:12:44 +0200

    split code for flip-viewport and flip-workspace call before/after-edge-action-hook at proper places removed (now unrequired) before-flip/after-flip helpers

--- lisp/sawfish/wm/edge/flip.jl
+++ lisp/sawfish/wm/edge/flip.jl
@@ -37,66 +37,74 @@
   (define-structure-alias edge-flip sawfish.wm.edge.flip)
 
   (define edge-flip-timer nil)
+  (define steps nil)
 
-  (define (edge-flip-invoke edge type #!key while-moving)
+  (define (edge-flip-invoke edge type while-moving)
     (unless edge-flip-timer
+
+      (call-hook 'before-edge-action-hook (list type edge while-moving))
+
       (setq edge-flip-timer
             (make-timer (lambda ()
                           (setq edge-flip-timer nil)
-                          (flip-core edge type #:while-moving while-moving))
+                          (if (eq type 'flip-viewport)
+                              (flip-viewport edge while-moving)
+                            (flip-workspace edge while-moving)))
                         (quotient edge-flip-delay 1000)
                         (mod edge-flip-delay 1000)))))
 
-  (define steps nil)
-
-  (define (flip-core edge type #!key while-moving)
+  (define (flip-viewport edge while-moving)
     (let ((ptr (query-pointer t)))
-      (before-flip)
-      (if (eq type 'viewport)
-	  (progn
-	    (if while-moving
-	        (setq steps 1)
-	      (setq steps scroll-viewport-steps))
-	    (let ((scroll-viewport-steps steps))
-	      (cond ((eq edge 'left)
-		    (when (move-viewport -1 0)
-		      (rplaca ptr (- (screen-width) 2))))
-		    ((eq edge 'right)
-		    (when (move-viewport 1 0)
-		      (rplaca ptr 1)))
-		    ((eq edge 'top)
-		    (when (move-viewport 0 -1)
-		      (rplacd ptr (- (screen-height) 2))))
-		    ((eq edge 'bottom)
-		    (when (move-viewport 0 1)
-		      (rplacd ptr 1))))
-	      (warp-cursor (car ptr) (cdr ptr))))
-	(let ((orig current-workspace))
-	  (cond ((eq edge 'left)
-		 (workspace-left)
-		 (rplaca ptr (- (screen-width) 2)))
-		((eq edge 'right)
-		 (workspace-right)
-		 (rplaca ptr 1))
-		((eq edge 'top)
-		 (workspace-up)
-		 (rplacd ptr (- (screen-height) 2)))
-		((eq edge 'bottom)
-		 (workspace-down)
-		 (rplacd ptr 1)))
-	  (unless (= current-workspace orig)
-	    (warp-cursor (car ptr) (cdr ptr)))
-	  (after-flip)))))
 
-  (define original-space)
+      (if while-moving
+	  (setq steps 1)
+	(setq steps scroll-viewport-steps))
 
-  (define (before-flip)
-    (when move-resize-window
-      (setq original-space current-workspace)))
+      (let ((scroll-viewport-steps steps))
+	(cond ((eq edge 'left)
+	      (when (move-viewport -1 0)
+		(rplaca ptr (- (screen-width) 2))))
+	      ((eq edge 'right)
+	      (when (move-viewport 1 0)
+		(rplaca ptr 1)))
+	      ((eq edge 'top)
+	      (when (move-viewport 0 -1)
+		(rplacd ptr (- (screen-height) 2))))
+	      ((eq edge 'bottom)
+	      (when (move-viewport 0 1)
+		(rplacd ptr 1))))
 
-  (define (after-flip)
-    (let ((w move-resize-window))
-      (when w
-	(when (and (/= original-space current-workspace)
-		   (not (window-get w 'sticky)))
-	  (move-window-to-workspace w original-space current-workspace t))))))
+	(warp-cursor (car ptr) (cdr ptr))
+
+        (call-hook 'after-edge-action-hook (list 'flip-viewport edge while-moving)))))
+
+  (define (flip-workspace edge while-moving)
+    (let ((ptr (query-pointer t))
+          (original-workspace current-workspace)
+          (mr-win))
+
+      (call-hook 'before-edge-action-hook (list 'flip-workspace edge while-moving))
+
+      (when while-moving
+        (setq mr-win move-resize-window))
+
+      (cond ((eq edge 'left)
+	      (workspace-left)
+	      (rplaca ptr (- (screen-width) 2)))
+	    ((eq edge 'right)
+	      (workspace-right)
+	      (rplaca ptr 1))
+	    ((eq edge 'top)
+	      (workspace-up)
+	      (rplacd ptr (- (screen-height) 2)))
+	    ((eq edge 'bottom)
+	      (workspace-down)
+	      (rplacd ptr 1)))
+
+      (unless (= current-workspace original-workspace)
+	(warp-cursor (car ptr) (cdr ptr))
+        (when (and mr-win
+	      (not (window-get mr-win 'sticky)))
+	  (move-window-to-workspace mr-win original-workspace current-workspace t)))
+
+      (call-hook 'after-edge-action-hook (list 'flip-workspace edge while-moving)))))
