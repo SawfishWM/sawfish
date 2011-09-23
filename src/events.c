4263bfc8b6d26323ad69e7a24c8518decb868d87
Christopher Roy Bratusek <nano@tuxfamily.org>
2011-09-23 18:02:18 +0200

    debug from mmc now imported, so:
    Revert "don't call not-yet imported debugging"
    
    This reverts commit 31f0f36d59fa30734b5fb096a836503d952590bf.
    
    Conflicts:
    
    	src/events.c

--- src/events.c
+++ src/events.c
@@ -673,7 +673,9 @@ property_notify (XEvent *ev)
 	    && w->property_change != 0 && !WINDOW_IS_GONE_P (w))
 	{
             repv tem;
-            tem = Fwindow_get (w_, Qrefresh, Qnil); /* fixme:  w -> w_ */
+            if  (debug)
+                DB (("%s: need_refresh property_change",  __FUNCTION__));
+            tem = Fwindow_get (w_, Qrefresh); /* fixme:  w -> w_ */
             if (!(tem && tem != Qnil))
             {
                 /* mmc: i think i raised this on ML year(s) ago... */
