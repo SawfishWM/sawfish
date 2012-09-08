5c7f10b43a4ca09c6503db0b00856fc2de3e2637
Christopher Roy Bratusek <nano@tuxfamily.org>
2012-09-08 12:18:44 +0200

    added sawfish.wm.prg.xsettingsd

---
+++ lisp/sawfish/wm/prg/xsettingsd.jl
@@ -0,0 +1,64 @@
+;; xsettingsd.jl -- xsettingsd integration
+
+;; Copyright (C) 2012 Christopher Roy Bratusek <nano@tuxfamily.org>
+
+;; This file is part of sawfish.
+
+;; sawfish is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; sawfish is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with sawfish; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301 USA.
+
+(define-structure sawfish.wm.prg.xsettingsd
+
+  (export start-xsettingsd
+          stop-xsettingsd
+          dump-xsettings)
+
+  (open rep
+        rep.system
+        rep.io.processes
+        rep.io.timers
+	rep.io.files
+        rep.util.misc
+        sawfish.wm.misc)
+
+  (define-structure-alias xsettingsd sawfish.wm.prg.xsettingsd)
+
+  (define %xsettingsd-proc nil)
+
+  ;; SAWFISHRC
+  ;; (require 'sawfish.wm.prg.xsettingsd)
+  ;; (add-hook 'after-initialization-hook start-xsettingsd t)
+
+  (define (dump-xsettings #!key (config (concat (getenv "HOME") "/.xsettingsd")))
+    (if (program-exists-p "dump_xsettings")
+	(system (format nil "dump_xsettings > %s &" config))
+      (message (format nil "dump_xsettings executable not found in PATH."))))
+
+  (define (start-xsettingsd #!key (config (concat (getenv "HOME") "/.xsettingsd")))
+    "Start xsettingsd. If a xsettingsd process already exists, it's beeing killed.
+     Configuration-file may be passed, if not, $HOME/.xsettingsd is used."
+    (if (program-exists-p "xsettingsd")
+        (progn
+	  (when %xsettingsd-proc (kill-process %xsettingsd-proc))
+          (setq %xsettingsd-proc (make-process))
+          (if (file-exists-p config)
+	      (start-process %xsettingsd-proc "xsettingsd" config)
+	    (display-message (format nil "given configuration filie does not exist.
+\n use (dump-xsettings) to generate default configuration."))))
+      (display-message (format nil "xsettingsd executable not found in PATH."))))
+
+  (define (stop-xsettingsd)
+    "Stop xsettingsd, if running."
+    (when %xsettingsd-proc (kill-process %xsettingsd-proc))))
