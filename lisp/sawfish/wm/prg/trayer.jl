74c8fff3ff9ae90cfaa29b461aa79c24069cae36
Christopher Roy Bratusek <nano@tuxfamily.org>
2012-09-09 11:22:30 +0200

    dozen of defcustoms for trayer

--- lisp/sawfish/wm/prg/trayer.jl
+++ lisp/sawfish/wm/prg/trayer.jl
@@ -29,49 +29,158 @@
         rep.io.processes
         rep.io.timers
 	rep.util.misc
-        sawfish.wm.misc)
+        sawfish.wm.misc
+	sawfish.wm.custom)
 
   (define-structure-alias trayer sawfish.wm.prg.trayer)
 
   (define %trayer-proc nil)
 
-  ;; SAWFISHRC
-  ;; (require 'sawfish.wm.prg.trayer)
-  ;; (add-hook 'after-initialization-hook start-trayer t)
+  (defcustom init-trayer nil
+    "Whether to start trayer with Sawfish."
+    :type boolean
+    :group (misc apps))
+
+  (defcustom trayer-expand 'false
+    "Wether to expand trayer."
+    :type (choice true false)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-edge 'top
+    "What edge to place trayer at."
+    :type (choice top right bottom left none)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-align 'left
+    "Alignment of trayer."
+    :type (choice left center right)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-widthtype 'percent
+    "Unit for trayer's width."
+    :type (choice request pixel percent)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-width 15
+    "Width for trayer."
+    :type number
+    :depends init-trayer
+    :range (0 . 2000)
+    :group (misc apps))
+
+  (defcustom trayer-heighttype 'pixel
+    "Unit for trayer's height."
+    :type (choice request pixel percent)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-height 28
+    "Height for trayer."
+    :type number
+    :range (0 . 1000)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-margin 0
+    "Margins for trayer."
+    :type number
+    :range (0 . 50)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-setdocktype 'false
+    "Whether to set trayer's window-type to dock."
+    :type (choice true false)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-setpartialstrut 'false
+    "Whether to set partial strut."
+    :type (choice true false)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-transparent 'false
+    "Whether to make trayer transparent."
+    :type (choice true false)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-alpha 0
+    "Alpha channel value for trayer."
+    :type (range (0 . 256))
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-tint 0
+    "Tint value for trayer."
+    :type (range (0 . 100))
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-distance 0
+    "Distance value for trayer."
+    :type number
+    :range (0 . 200)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-distancefrom 0
+    "DistanceFrom value for trayer."
+    :type number
+    :range (0 . 200)
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-padding 0
+    "Padding value for trayer."
+    :type (range (0 . 100))
+    :depends init-trayer
+    :group (misc apps))
+
+  (defcustom trayer-monitor 0
+    "Monitor to display trayer on."
+    :type number
+    :range (0 . 16)
+    :depends init-trayer
+    :group (misc apps))
 
   (define (start-trayer #!key
-			(expand "false")
-			(edge "top")
-			(align "left")
-			(widthtype "percent")
-			(width 15)
-			(heighttype "pixel")
-			(height 28)
-			(margin 0)
-			(setdocktype "false")
-			(setpartialstrut "false")
-			(transparent "false")
-			(alpha 0)
-			(tint 0)
-			(distance 0)
-			(distancefrom 0)
-			(padding 0)
-			(monitor 0))
+			(expand trayer-expand)
+			(edge trayer-edge)
+			(align trayer-align)
+			(widthtype trayer-widthtype)
+			(width trayer-width)
+			(heighttype trayer-heighttype)
+			(height trayer-height)
+			(margin trayer-margin)
+			(setdocktype trayer-setdocktype)
+			(setpartialstrut trayer-setpartialstrut)
+			(transparent trayer-transparent)
+			(alpha trayer-alpha)
+			(tint trayer-tint)
+			(distance trayer-distance)
+			(distancefrom trayer-distancefrom)
+			(padding trayer-padding)
+			(monitor trayer-monitor))
     "Start trayer. If a trayer process already exists, it's beeing killed.
-     All options of trayer are supported. Note that boolean values must be
-     passed as strings (true/false), not booleans (t/nil)."
+All options of trayer are supported."
     (if (program-exists-p "trayer")
         (progn
 	  (when %trayer-proc (kill-process %trayer-proc))
 	  (setq %trayer-proc (make-process))
-	  (start-process %trayer-proc "trayer" "--expand" expand "--edge" edge
-            "--align" align "--widthtype" widthtype "--width" (number->string width)
-            "--heighttype" heighttype "--height" (number->string height)
-            "--SetDockType" setdocktype "--margin" (number->string margin)
-            "--SetPartialStrut" setpartialstrut "--monitor" (number->string monitor)
+	  (start-process %trayer-proc "trayer" "--expand" (format nil "%s" expand) "--edge" (format nil "%s" edge)
+            "--align" (format nil "%s" align) "--widthtype" (format nil "%s" widthtype) "--width" (number->string width)
+            "--heighttype" (format nil "%s" heighttype) "--height" (number->string height)
+            "--SetDockType" (format nil "%s" setdocktype) "--margin" (number->string margin)
+            "--SetPartialStrut" (format nil "%s" setpartialstrut) "--monitor" (number->string monitor)
             "--alpha" (number->string alpha) "--tint" (number->string tint)
             "--distance" (number->string distance) "--distancefrom" (number->string distancefrom)
-            "--padding" (number->string padding) "--transparent" transparent))
+            "--padding" (number->string padding) "--transparent" (format nil "%s" transparent)))
       (display-message (format nil "trayer executable was not found in PATH."))))
 
   (define (stop-trayer)
