cc2e49ba6e9cb91d6ad4622f6d691adef777773b
Christopher Roy Bratusek <nano@jpberlin.de>
2015-07-28 18:52:45 +0200

    properly filter out Conky from Cabinet

--- lisp/sawfish/wm/ext/cabinet.jl
+++ lisp/sawfish/wm/ext/cabinet.jl
@@ -183,7 +183,7 @@
   
   (defcustom cabinet:filter-out-from-display (list "^Gdesklets-daemon$" "^Xfce4-panel$" "^Mate-panel$"
                                                    "^Xfdesktop$" "^x-caja-desktop$" "^plasma-desktop$"
-						   "^Pancake$" "^lxpanel" "Sawfishpager" "Conky"
+						   "^Pancake$" "^lxpanel" "Sawfishpager" "^conky$"
 						   "Lumina Desktop Environment")
     nil
     :group (focus cabinet filter)
