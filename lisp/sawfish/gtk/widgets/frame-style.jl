1b17ab7e83474d9a28786f836b7c184ba97b634d
Christopher Roy Bratusek <nano@jpberlin.de>
2017-08-25 22:35:48 +0200

    simplify sawfish.gtk.widgets.frame-style README parsing (OpenBSD patch)

--- lisp/sawfish/gtk/widgets/frame-style.jl
+++ lisp/sawfish/gtk/widgets/frame-style.jl
@@ -100,9 +100,7 @@
 					  (setq full dir)
 					  (throw 'out t))
 				      (error))))
-				'("%s" "%s.tar#tar/%s" "%s.tar.gz#tar/%s"
-				  "%s.tar.Z#tar/%s" "%s.tar.bz2#tar/%s"
-				  "%s.tar.lzma#tar/%s" "%s.tar.xz#tar/%s"))
+				'("%s"))
 			  nil)
 		    (setq full (i18n-filename
 				(expand-file-name "README" full)))
